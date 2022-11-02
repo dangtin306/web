58de03236bf27824a9bc54956b42a37862a296f5
dangtin306 <dangtin306@gmail.com>
2022-11-03 01:47:08 +0700

    asdasdasdsad

--- resources/views/appvue.blade.php
+++ resources/views/appvue.blade.php
@@ -15,7 +15,8 @@ use Illuminate\Support\Facades\DB;
         <meta charset="utf-8">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" type="text/css"  href="/css/app.css">
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+      
         <title>OK</title>
 
         <!-- Fonts -->
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -80,6 +80,7 @@ echo ("Tải khoản của bạn là : ".$username)  ;
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         {{-- <link rel="stylesheet" type="text/css" href="/css/app.css"> --}}
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
         <title>Hust Media</title>
 
         <!-- Fonts -->
