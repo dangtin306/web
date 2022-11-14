8bfc9abfb0cb89dbaebac972a5960e6b10691028
dangtin306 <dangtin306@gmail.com>
2022-11-14 16:23:50 +0700

    ádasdsadádsdaasd

--- resources/js/congdong/profile.vue
+++ resources/js/congdong/profile.vue
@@ -43,11 +43,80 @@
         </div>
 
     </div>
+    <div v-if="optionprofile != null" class="col mt-0">
+        
+        <div  class='mt-4 mx-2 items-center justify-center from-teal-100 via-teal-300 to-teal-500 '>
+            <div class="ml-3 h-7 flex justify-end items-center">
+                <button @click="tatprofile"  type="button"
+                    class="bg-gray-100 dark:bg-gray-700 m-1 p-3 justify-end rounded-md text-gray-400 hover:text-gray-500 focus:ring-2 focus:ring-indigo-500">
+                
+                    <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg"
+                        fill="none" viewBox="0 0 24 24" stroke="currentColor"
+                        aria-hidden="true">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                </button>
+             
+            </div>
+          <div  class='w-full max-w-lg px-10 py-8 mx-auto bg-white rounded-lg shadow-xl'>
+             
+              <div class='max-w-md mx-auto  space-y-6'>
+             
+                    <div class="col-lg-6">
+                        <div class="form-group">
+                        
+                          <label for="countries2" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Chọn lựa chọn</label>
+          
+                    <select  id="countries2" class="
+                    selectpicker sp1 form-control"    name="nhamang" v-model="chedoprofile">
+                        <option v-for="option in options" :key="option.value" :value="option.value">
+    {{ option.text }}
+  </option>
+                      </select>
+    
+                  
+                    
+                        </div>
+                      </div>
+       
+                      <p>Tên tài khoản hoặc mật khẩu cần thay</p>
+                      <input class="form-control form-control-alternative" v-model="lienketchay" placeholder="Nhập cái bạn cần nhé =)))" />
+                      <div v-if="thanhcong">  
+    <div class="relative py-3 sm:max-w-xl sm:mx-auto">
+      <div class="flex flex-col items-center justify-center py-2">
+        <div class="absolute inset-0 bg-gradient-to-r from-indigo-200 to-purple-600 shadow-lg transform rotate-6 rounded-3xl">
 
+        </div>
+        <div class="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
+         <h2 class="text-3xl text-break font-bold">
+          kết quả {{ thanhcong }}</h2></div></div></div>
+    </div>
+                      <button  @click="xacnhan"
+                      class='flex break-inside bg-purple-400 hover:bg-purple-300 rounded-3xl px-8 py-2 mb-3 w-full dark:bg-slate-800 dark:text-white'>
+                      <div class='flex items-center justify-between flex-1'>
+                        <div v-if="nutxuly == '1'" class="spinner-border" role="status">
+                       
+                        </div>
+                     
+                        <span class='text-lg font-medium text-white'> {{ nutorder }} </span>
+                        <svg  width='17' height='17' viewBox='0 0 17 17' fill='none' xmlns='http://www.w3.org/2000/svg'>
+                          <path fillRule='evenodd' clipRule='evenodd'
+                            d='M0 8.71423C0 8.47852 0.094421 8.25246 0.262491 8.08578C0.430562 7.91911 0.658514 7.82547 0.896201 7.82547H13.9388L8.29808 2.23337C8.12979 2.06648 8.03525 1.84013 8.03525 1.60412C8.03525 1.36811 8.12979 1.14176 8.29808 0.974875C8.46636 0.807989 8.6946 0.714233 8.93259 0.714233C9.17057 0.714233 9.39882 0.807989 9.5671 0.974875L16.7367 8.08499C16.8202 8.16755 16.8864 8.26562 16.9316 8.3736C16.9767 8.48158 17 8.59733 17 8.71423C17 8.83114 16.9767 8.94689 16.9316 9.05487C16.8864 9.16284 16.8202 9.26092 16.7367 9.34348L9.5671 16.4536C9.39882 16.6205 9.17057 16.7142 8.93259 16.7142C8.6946 16.7142 8.46636 16.6205 8.29808 16.4536C8.12979 16.2867 8.03525 16.0604 8.03525 15.8243C8.03525 15.5883 8.12979 15.362 8.29808 15.1951L13.9388 9.603H0.896201C0.658514 9.603 0.430562 9.50936 0.262491 9.34268C0.094421 9.17601 0 8.94995 0 8.71423Z'
+                            fill='white' />
+                        </svg>
+                      </div>
+                    </button>
+            </div>
+       
+            </div>
+            <br>
+          </div>
+        </div>
     <div
         class="grid rounded-2xl divide-y divide-dashed hover:divide-solid  justify-evenly bg-gray-50 dark:bg-gray-300 m-3 mt-10 grid-cols-3">
-        <div class="col-span-1  p-3">
-            <div class="flex flex-col items-center ">
+          <div class="col-span-1  p-3">
+            <div @click="click('username')" class="flex flex-col items-center ">
                <button
                         class="tr-300">
                         <svg xmlns="http://www.w3.org/2000/svg"
@@ -58,15 +127,13 @@
                                 d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
                         </svg>
                         <span class="text-lg font-medium">Đổi tên tài khoản
-
-                            <br> Tính năng chưa có
                         </span>
                     </button>
             </div>
         </div>
         <div class="col-span-1  p-3">
             <div class="flex flex-col items-center ">
-                <a href="https://hust.media/profile/"> <button
+                <div @click="click('password')"> <button
                         class="tr-300">
                         <svg xmlns="http://www.w3.org/2000/svg"
                             class="h-14 w-14 text-gray-500" fill="none"
@@ -76,12 +143,12 @@
                                 d="M3 10h18M7 15h1m4 0h1m-7 4h12a3 3 0 003-3V8a3 3 0 00-3-3H6a3 3 0 00-3 3v8a3 3 0 003 3z" />
                         </svg>
                         <span class="text-lg font-medium">Đổi mật khẩu</span>
-                    </button></a>
+                    </button></div>
             </div>
         </div>
         <div class="col-span-1  p-3">
             <div class="flex flex-col items-center ">
-                <a href="https://hust.media/profile/"> <button
+                <div @click="click('aoikey')"><button
                         class="tr-300">
                         <svg xmlns="http://www.w3.org/2000/svg"
                             class="h-14 w-14 text-gray-500" fill="none"
@@ -91,12 +158,12 @@
                                 d="M11 5.882V19.24a1.76 1.76 0 01-3.417.592l-2.147-6.15M18 13a3 3 0 100-6M5.436 13.683A4.001 4.001 0 017 6h1.832c4.1 0 7.625-1.234 9.168-3v14c-1.543-1.766-5.067-3-9.168-3H7a3.988 3.988 0 01-1.564-.317z" />
                         </svg>
                         <span class="text-lg font-medium">Apikey</span>
-                    </button></a>
+                    </button></div> 
             </div>
         </div>
         <div class="col-span-1  p-3">
             <div class="flex flex-col items-center ">
-                <a href="https://hust.media/profile/">
+                <a href="https://hust.media/profile/#here">
                     <button class="tr-300">
                         <svg xmlns="http://www.w3.org/2000/svg"
                             class="h-14 w-14 text-gray-500" fill="none"
@@ -146,13 +213,25 @@
     </div>
 </template>
 <script>
+import axios from 'axios' ;
+import Swal from 'sweetalert2' ;
   export default {
     data (){
           return {
+            optionprofile: null ,
             ok2 : this.$cookies.get("apikey") ,
             money : null ,
             chietkhau: null ,
+            nutorder: 'Ấn đây để xác nhận' ,
             experience: null ,
+            thanhcong : '' ,
+            nutxuly: 0 ,
+            options: [
+                    { text: 'Vui lòng chọn 1 lựa chọn', value: 'vuilongchon1cai' } ,
+        { text: 'Thay mật khẩu', value: 'password' } , 
+        { text: 'Lấy api key', value: 'apikey' } ,
+        { text: 'Thay tên tài khoản', value: 'username' }
+      ],
             total_nap: null 
           }},
           created() {
@@ -173,12 +252,84 @@
 
         },
           methods : {
+            tatprofile()
+        {
+            this.optionprofile = null ;
+        },
             setCookie(cname, cvalue, exdays) {
     var d = new Date();
     d.setTime(d.getTime() + (exdays*24*60*60*1000));
     var expires = "expires="+ d.toUTCString();
     document.cookie = cname + "=" + cvalue + "; " + expires;
 },
+click(optionprofile)
+            {
+                setTimeout(() => {    
+                    $('.selectpicker').selectpicker('refresh');
+                    setTimeout(() => {     
+         $('.sp1').selectpicker('toggle');  }, 300)
+                }, 300) ;
+this.optionprofile = optionprofile ;
+
+            } ,
+            xacnhan(){
+            this.nutxuly = 1 ;
+            this.nutorder = 'chờ xíu nhé' ;
+            if ( this.chedoprofile  == null  ||  this.chedoprofile  == 'vuilongchon1cai')
+        {
+            this.nutxuly = 0 ,
+            this.nutorder = 'Xác nhận' ,
+            Swal.fire({
+  title: 'Vui lòng chọn lựa chọn của bạn' ,
+  heightAuto : false,
+ 
+})
+        } 
+        else if ( this.chedoprofile  == 'apikey' )
+        {this.nutxuly = 0 ,
+            this.nutorder = 'Xác nhận' ,
+this.thanhcong =  this.ok2 ;
+        }
+        else
+        {
+            axios
+         .post('https://tuongtac.fun/api/profile.php', {
+          apikey: this.ok2 ,
+          chedo: 'profile' ,
+          option: this.chedoprofile ,
+      lienketchay: this.lienketchay
+    })
+    .then( response => (
+    console.log(response.data ) ,
+    this.dathang(response )
+    ))
+    .catch(error => console.log(error) , ) ;
+        }
+  
+  
+          },
+          dathang(response){
+this.nutorder = 'xác nhận' ,
+this.nutxuly = 0 ,
+this.info = response.data ,
+      this.status = this.info.status     ,
+      this.message = this.info.message  
+      if ( this.status == 0  )
+      {
+        Swal.fire({
+  title: this.message ,
+  heightAuto : false,
+ 
+});
+      }
+      else
+      {
+        this.thanhcong =  this.message ;
+      }
+       
+
+          } ,
+        
 testFunction4(response)
   {
    
