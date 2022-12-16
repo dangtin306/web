b82163e6584bee1c768c76ae1e205cd38a7f9e9b
dangtin306 <dangtin306@gmail.com>
2022-12-16 15:47:12 +0700

    dfdfdffdsadsad

--- resources/js/truyen/truyen.vue
+++ resources/js/truyen/truyen.vue
@@ -102,7 +102,7 @@ import VRuntimeTemplate from "vue3-runtime-template";
   
       created() {
         setTimeout(function(){
-    $('#loadingg').hide();}, 14000) ;
+    $('#loadingg').hide();}, 17000) ;
     this.laymeta();
           this.fetchData();
           this.$watch(
@@ -143,7 +143,7 @@ $('#loadingg').hide();
 var splitter = '/truyen-tranh/' ;
 
 let config = {
-    timeout: 17000,
+    timeout: 22000,
 };
 var urlsplit = url.split(splitter)[1];
 urlsplit = 'https://tecom.pro/truyen-tranh/apidoctruyen.php/' + urlsplit ;
@@ -163,7 +163,7 @@ console.log(urlsplit);
 var splitter = '/truyen-tranh/' ;
 
 let config = {
-    timeout: 17000,
+    timeout: 22000,
 };
 var urlsplit = url.split(splitter)[1];
 urlsplit = 'https://tecom.pro/truyen-tranh/metadoctruyen.php/' + urlsplit ;
