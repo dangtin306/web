c026da23a74e5cc170c9325b43aadaae2dcba1c8
dangtin306 <dangtin306@gmail.com>
2023-01-26 15:34:43 +0700

    ádadsadsadsdaacscascacsa

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -227,7 +227,7 @@ data-template="vertical-menu-template-free"
          </router-link>
       </li>
       <li class="menu-item">
-        <router-link to="/truyen-tranh/home" class="menu-link">
+        <router-link  to="/truyen-tranh/home" class="menu-link layout-menu-toggle">
         <img src="https://tuongtac.fun/img/icon/zero-two-icon.gif" style="width:27px;height:27px">
         <div data-i18n="Basic">Đọc truyện tranh</div>
       </router-link>
