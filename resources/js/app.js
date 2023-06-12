988ced960d25b9019b5c362ad13f42d136877001
dangtin306 <dangtin306@gmail.com>
2023-06-12 20:18:28 +0700

    sadsadasdsdvsdvsvdsdv

--- resources/js/app.js
+++ resources/js/app.js
@@ -16,7 +16,7 @@ const metaManager = createMetaManager(false, {
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
+ * components and automatically register thdoitheem with their "basename".
  *
  * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
  */
