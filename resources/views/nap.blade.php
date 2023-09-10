2e56f9ae0b5910281c9c34774ccbf8f049a37059
dangtin306 <dangtinn306@gmail.com>
2023-09-10 23:31:33 +0700

    fghghfghfhgfẻgerg

--- resources/views/nap.blade.php
+++ resources/views/nap.blade.php
@@ -90,9 +90,9 @@ echo ("Tải khoản của bạn là : ".$username)  ;
         </div>     
         <script src="https://js.pusher.com/7.2/pusher.min.js"></script>
 
-        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="/js/jquery-3.3.1.slim.min.js" ></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+        <script src="/js/bootstrap.min.js" ></script>
           
         <script src="/js/app.js"></script>
         <script src="https://tuongtac.fun/okok/assets/vendor/js/helpers.js"></script>
