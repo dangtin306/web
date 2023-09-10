860c3419f549730948e612260b584091ab5faee7
dangtin306 <dangtinn306@gmail.com>
2023-09-10 23:33:01 +0700

    regergregaádadsasd

--- resources/views/nap.blade.php
+++ resources/views/nap.blade.php
@@ -91,7 +91,7 @@ echo ("Tải khoản của bạn là : ".$username)  ;
         <script src="https://js.pusher.com/7.2/pusher.min.js"></script>
 
         <script src="/js/jquery-3.3.1.slim.min.js" ></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+        <script src="/js/popper.min.js" ></script>
         <script src="/js/bootstrap.min.js" ></script>
           
         <script src="/js/app.js"></script>
--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -30,7 +30,7 @@ echo  $content ;
             <profile></profile> 
         </div>     
         <script src="/js/jquery-3.3.1.slim.min.js" ></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+        <script src="/js/popper.min.js" ></script>
         <script src="/js/bootstrap.min.js" ></script>
           
         <script src="/js/app.js"></script>
