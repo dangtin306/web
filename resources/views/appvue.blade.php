58e8f0bed761acaf9f724e081dfb92a4c2a92fb0
dangtin306 <dangtinn306@gmail.com>
2023-11-22 16:37:27 +0700

    ádasdasdasddsaasdádasdasdsadsad

--- resources/views/appvue.blade.php
+++ resources/views/appvue.blade.php
@@ -53,7 +53,7 @@ $(document).ready(function() {
     <meta name="viewport" content="width=device-width, initial-scale=1">
     {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script> --}}
-    <link href="{{ asset('css/app.css?v=2.3.5') }}" rel="stylesheet">
+    <link href="{{ asset('css/app.css?v=2.3.6') }}" rel="stylesheet">
 
     {{-- <title>OK</title> --}}
     <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-123456789"
@@ -77,7 +77,7 @@ $(document).ready(function() {
     <script src="/js/popper.min.js"></script>
     <script src="/js/bootstrap.min.js"></script>
 
-    <script src="/js/app.js?v=2.3.5"></script>
+    <script src="/js/app.js?v=2.3.6"></script>
     <script src="https://tecom.pro/okok/assets/vendor/js/helpers.js"></script>
     <script src="https://tecom.pro/okok/assets/js/config.js"></script>
     <script src="https://tecom.pro/okok/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
